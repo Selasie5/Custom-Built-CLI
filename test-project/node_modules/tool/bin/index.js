644a968fcb433a6ae61bf7c8b5fa6d7fd53b6dbe
Selasie5 <selasisepenu5@gmail.com>
2024-09-12 21:28:03 +0100

    Implement try catch to properly handle error properly

--- test-project/node_modules/tool/bin/index.js
+++ test-project/node_modules/tool/bin/index.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 //Hashabang directive stating the interpreter we are using
-const arg = require('arg')
+const arg = require('arg');
 
-const args = arg({
-    '--start': Boolean,
-    '--build': Boolean
-})
-
-if(args(['--start'])){
-    console.log('Starting the application')
+try {
+    const args = arg({
+        '--start': Boolean,
+        '--build': Boolean
+    })
+    
+    if(args['--start']){
+        console.log('Starting the application')
+    }
+    console.log(process.argv)
+} catch (error) {
+    console.log(error.message)
 }
-console.log(process.argv)

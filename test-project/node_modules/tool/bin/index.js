907ac58cb26d69cd7c26bf306b7293fba48a9015
Selasie5 <selasisepenu5@gmail.com>
2024-09-12 22:02:14 +0100

    Implement basic configuration for user

--- test-project/node_modules/tool/bin/index.js
+++ test-project/node_modules/tool/bin/index.js
@@ -2,6 +2,8 @@
 //Hashabang directive stating the interpreter we are using
 import arg from 'arg'
 import chalk from 'chalk'
+import path from "path"
+import pkgUp, { pkgUpSync } from "pkg-up"
 
 try {
     const args = arg({
@@ -10,6 +12,16 @@ try {
     })
     
     if(args['--start']){
+        // const pkg = require(path.join(process.cwd(),'package.json')) //If the user runs our tool from a subdirectory it will throw an error
+        //REFACTOR
+        const pkgPath= pkgUpSync({cwd:process.cwd()})
+        const pkg = require(pkgPath);
+        if(pkg.tool){
+            console.log("Found configuration", pkg.tool)
+        }
+        else{
+            console.log(chalk.yellowBright('Could not find configuration using default'))
+        }
         console.log(chalk.bgCyanBright('Starting the application'))
     }
     console.log(process.argv)
@@ -22,7 +34,7 @@ try {
 function usage(){
     console.log(`
         tool[CMD]
-        --start\tStarts the app
-        --build\tBuilds the app
+       ${chalk.greenBright('--start')}\tStarts the app
+       ${chalk.greenBright('--build')}\tBuilds the app
         `)
 }

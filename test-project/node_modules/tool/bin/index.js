f1c080a746a34ad14a2b2a268fb7754a776c6bbf
Selasie5 <selasisepenu5@gmail.com>
2024-09-12 22:58:58 +0100

    Refactor code for modularity

--- test-project/node_modules/tool/bin/index.js
+++ test-project/node_modules/tool/bin/index.js
@@ -2,8 +2,8 @@
 //Hashabang directive stating the interpreter we are using
 import arg from 'arg'
 import chalk from 'chalk'
-import path from "path"
-import pkgUp, { pkgUpSync } from "pkg-up"
+import { getConfig } from '../src/config/config-mgr'
+import { start } from '../src/commands/start'
 
 try {
     const args = arg({
@@ -12,17 +12,8 @@ try {
     })
     
     if(args['--start']){
-        // const pkg = require(path.join(process.cwd(),'package.json')) //If the user runs our tool from a subdirectory it will throw an error
-        //REFACTOR
-        const pkgPath= pkgUpSync({cwd:process.cwd()})
-        const pkg = require(pkgPath);
-        if(pkg.tool){
-            console.log("Found configuration", pkg.tool)
-        }
-        else{
-            console.log(chalk.yellowBright('Could not find configuration using default'))
-        }
-        console.log(chalk.bgCyanBright('Starting the application'))
+       const config =getConfig();
+       start(config);
     }
     console.log(process.argv)
 } catch (error) {
